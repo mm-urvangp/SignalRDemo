70dc586d3ac976f94fab249bb2daf12241eb8ad8
Rutul Patel <rutul@magnusminds.net>
2024-12-09 19:03:50 +0530

    Add project files.

---
+++ Hubs/UserHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo.Hubs
+{
+    public class UserHub : Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public static int TotalUsers { get; set; } = 0;
+
+        public override Task OnConnectedAsync()
+        {
+            TotalUsers++;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            TotalUsers--;
+            Clients.All.SendAsync("updateTotalUsers", TotalUsers).GetAwaiter().GetResult();
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            //Send update to all clients that TotalViews have been Updated
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+
+    }
+}
---
+++ wwwroot/js/userCount.js
@@ -0,0 +1,36 @@
+﻿// Create Connection
+var connectionUserCount = new signalR.HubConnectionBuilder().withUrl("/hubs/userCount").build();
+
+
+//connect to methods that hub invokes aka receive notification from hub
+connectionUserCount.on("updateTotalViews", (value) => {
+    var newCountSpan = document.getElementById("totalViewsCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+connectionUserCount.on("updateTotalUsers", (value) => {
+    var newCountSpan = document.getElementById("totalUsersCounter");
+    newCountSpan.innerText = value.toString();
+})
+
+
+//invoke hub methods aka send notification to hub
+function newWindowLoadedOnClient() {
+    connectionUserCount.send("NewWindowLoaded");
+}
+
+
+//start connection
+function fulFilled() {
+    //do something on start
+    console.log("Connection to User Hub Successfully");
+    newWindowLoadedOnClient();
+}
+
+function rejected() {
+    //rejected logs
+
+}
+
+
+connectionUserCount.start().then(fulFilled, rejected)
